d8387140e91d73baf6972c1ee03e5535f14c043d
oliverjessner <oliver.jessner@gmail.com>
2026-05-08 10:42:53 +0200

    fixing install bug

--- Formula/sqlite-hub.rb
+++ Formula/sqlite-hub.rb
@@ -3,11 +3,13 @@ class SqliteHub < Formula
   homepage "https://github.com/oliverjessner/sqlite-hub"
   url "https://github.com/oliverjessner/sqlite-hub/archive/refs/tags/v0.9.3.tar.gz"
   sha256 "e8b2aea6d94c53885062aaa21a188e4888af653d09fe0f2d8c76907b863d5b76"
+  revision 1
 
-  depends_on "node"
   depends_on "python" => :build
+  depends_on "node@24"
 
   def install
+    ENV.prepend_path "PATH", Formula["node@24"].opt_bin
     ENV["npm_config_build_from_source"] = "true"
     system "npm", "install", *std_npm_args
     cd libexec/"lib/node_modules/sqlite-hub" do
@@ -17,8 +19,11 @@ class SqliteHub < Formula
   end
 
   test do
+    node = Formula["node@24"].opt_bin/"node"
     output = shell_output(
-      "cd #{libexec}/lib/node_modules/sqlite-hub && #{Formula["node"].opt_bin}/node -e "       "'const Database = require(\"better-sqlite3\"); const db = new Database(\":memory:\"); "       "console.log(db.prepare(\"select 1 as value\").get().value); db.close();'"
+      "cd #{libexec}/lib/node_modules/sqlite-hub && #{node} -e " \
+      "'const Database = require(\"better-sqlite3\"); const db = new Database(\":memory:\"); " \
+      "console.log(db.prepare(\"select 1 as value\").get().value); db.close();'",
     )
     assert_equal "1\n", output
   end
