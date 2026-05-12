8a426456c456f64a5bb8b416b92d8a1fd74a0d73
oliverjessner <oliver.jessner@gmail.com>
2026-05-12 15:02:21 +0200

    itworksbut 0.5.0

--- Formula/itworksbut.rb
+++ Formula/itworksbut.rb
@@ -1,8 +1,8 @@
 class Itworksbut < Formula
   desc "Static CI scanner for JavaScript vibe-coding project risks"
   homepage "https://github.com/oliverjessner/ItWorksBut"
-  url "https://registry.npmjs.org/itworksbut/-/itworksbut-0.4.0.tgz"
-  sha256 "cdd041449759dcd259bd238ab577f558477249f04623c12620e09dd2ba6d05c2"
+  url "https://registry.npmjs.org/itworksbut/-/itworksbut-0.5.0.tgz"
+  sha256 "af4b30cd61742b7cd42f8bfd5276b0bf20573effd847837b0b007b3c3ae05b19"
   license "MIT"
 
   depends_on "node"
