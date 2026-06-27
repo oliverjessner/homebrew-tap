2585b512440506f0099f2b76a4036d39aabc350d
oliverjessner <oliver.jessner@gmail.com>
2026-06-28 00:37:22 +0200

    switching to swift

--- Formula/clipspin.rb
+++ Formula/clipspin.rb
@@ -1,8 +1,8 @@
 class Clipspin < Formula
   desc "Temporary second paste queue for macOS"
   homepage "https://github.com/oliverjessner/clipspin"
-  url "https://github.com/oliverjessner/clipspin/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "269420af44d3f8999dd1bf6327fb3d8090216e829f8dd1f54b4a913070fdedc3"
+  url "https://github.com/oliverjessner/clipspin/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "5a44e966c526ab300b694b22a4657c9ae81bf5973d840aa86a38586bfb220d24"
   license "MIT"
 
   depends_on xcode: :build
