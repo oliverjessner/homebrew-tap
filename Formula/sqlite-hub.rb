6bb8e059911669a19dac75c5aad1eb8c231d9e1f
oliverjessner <oliver.jessner@gmail.com>
2026-07-08 23:57:44 +0200

    sqlite-hub 2.1.0

--- Formula/sqlite-hub.rb
+++ Formula/sqlite-hub.rb
@@ -1,8 +1,8 @@
 class SqliteHub < Formula
   desc "SQLite-only local management app backend and SPA shell"
   homepage "https://github.com/oliverjessner/sqlite-hub"
-  url "https://github.com/oliverjessner/sqlite-hub/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "c4f40595ed3a7be57de8be4facdf3b0213979a744667a9041542f9a7c69a2a68"
+  url "https://github.com/oliverjessner/sqlite-hub/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "a590b6f2412b467aef9ebb8e461ddb7c81d629bf3f5f7dd40c98084c42916dc8"
 
   depends_on "node"
   depends_on "python" => :build
