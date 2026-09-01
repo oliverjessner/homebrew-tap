76c52eaadec181264b5a93b69bfe03abb06a15b5
oliverjessner <oliver.jessner@gmail.com>
2026-09-01 10:45:38 +0200

    fetchary 0.1.1

--- Formula/fetchary.rb
+++ Formula/fetchary.rb
@@ -1,8 +1,8 @@
 class Fetchary < Formula
   desc "Watch web pages for changes and archive exact response versions"
   homepage "https://github.com/oliverjessner/fetchary"
-  url "https://registry.npmjs.org/fetchary/-/fetchary-0.1.0.tgz"
-  sha256 "3a5283f3570353429d0ed100749a79908a32b7d6140e7cf1e88ccd427c5a0649"
+  url "https://registry.npmjs.org/fetchary/-/fetchary-0.1.1.tgz"
+  sha256 "883bff3d825be46410178d1380a98471b43514ece0d242a656d9528ca7f1fd50"
   license "MIT"
 
   depends_on "node"
